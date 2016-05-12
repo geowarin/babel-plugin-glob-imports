8875f973710ae33172162e15c640ee40c5bd0428
Geoffroy Warin <geoffroy.warin@mnt.fr>
2016-05-12 14:06:08 +0200

    Detect conflicting imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import { dirname, basename, extname } from 'path';
 import glob from 'glob';
 
+function ensureNamesUniqueness(sourcesWithNames, path, globValue) {
+  const srcByNames = {};
+  for (const src of sourcesWithNames) {
+    srcByNames[src.name] = (srcByNames[src.name] || []).concat(src);
+  }
+  for (const name of Object.keys(srcByNames)) {
+    if (srcByNames[name].length > 1) {
+      const paths = srcByNames[name].map(s => s.src).join(', ');
+      throw path.buildCodeFrameError(`Found conflicting imports named "${name}" from glob ${globValue}: ${paths}`)
+    }
+  }
+}
+
 export default function ({ types: t }) {
   return {
     visitor: {
@@ -11,8 +24,9 @@ export default function ({ types: t }) {
           return
         }
 
+        const globValue = source.value;
         const fromDir = dirname(file.file.opts.filename)
-        const sources = glob.sync(source.value, {
+        const sources = glob.sync(globValue, {
           cwd: fromDir,
           strict: true
         });
@@ -23,9 +37,12 @@ export default function ({ types: t }) {
             name: basename(source, ext)
           };
         });
+        ensureNamesUniqueness(sourcesWithNames, path, globValue);
+
+        const sourcesNames = sourcesWithNames.map(s => s.name);
 
         if (specifiers.length !== 1 || specifiers[0].type !== 'ImportDefaultSpecifier') {
-          throw path.buildCodeFrameError('Glob imports only works with ')
+          throw path.buildCodeFrameError('Glob imports only works with default import. e.g.: import myModule from "src/*.js"')
         }
 
         const importName = specifiers[0].local.name;
@@ -37,7 +54,6 @@ export default function ({ types: t }) {
         }
 
         const newImports = sourcesWithNames.map(s => makeImport(s.name, s.src));
-        const sourcesNames = sourcesWithNames.map(s => s.name);
         const replacements = [...newImports, makeObject(importName, sourcesNames)];
 
         path.replaceWithMultiple(replacements);
--- test/test.js
+++ test/test.js
@@ -20,4 +20,33 @@ const modules = {
   one: one,
   two: two
 };`);
-})
+});
+
+test('should import globs - nested dirs', t => {
+  const code = 'import modules from "./fixtures/nested/**/*.js"';
+  const result = transform(code);
+
+  t.is(result, `import * as one from "./fixtures/nested/one.js";
+import * as two from "./fixtures/nested/subdir/two.js";
+const modules = {
+  one: one,
+  two: two
+};`);
+});
+
+test('should throw in case of conflicts', t => {
+  const code = 'import modules from "./fixtures/conflicts/**/*.js"';
+  t.throws(
+    attemptToTransform(code),
+    checkError(`Found conflicting imports named "one" from glob ./fixtures/conflicts/**/*.js: ./fixtures/conflicts/one.js, ./fixtures/conflicts/subdir/one.js`)
+  );
+});
+
+function attemptToTransform (code) {
+  return new Promise(resolve => resolve(transform(code)))
+}
+
+function checkError (msg) {
+  const preface = 'glob-imports.js: '
+  return err => err instanceof SyntaxError && err.message.slice(0, preface.length) === preface && err.message.slice(preface.length) === msg
+}
